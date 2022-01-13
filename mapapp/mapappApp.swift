51239a575603c5571ac5200d290a7c7e1bd3b91a
bowwow2115 <bowwow2115@gmail.com>
2022-01-13 23:32:50 +0900

    Initial Commit

---
+++ mapapp/mapappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mapappApp.swift
+//  mapapp
+//
+//  Created by 박승훈 on 2022/01/13.
+//
+
+import SwiftUI
+
+@main
+struct mapappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
